d5ff8efb02de25dd23e9fc8502235933db7772c0
naadaa22 <farahmzoughi11@gmail.com>
2025-11-24 17:12:04 +0100

    I creat the pages just to see the project run then i'll fix it

---
+++ src/app/component/navbar/navbar.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatMenuModule } from '@angular/material/menu';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatMenuModule
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+  cartItemsCount = 0;
+  isLoggedIn = false;
+}
