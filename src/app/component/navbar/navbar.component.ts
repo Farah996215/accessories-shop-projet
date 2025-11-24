b23d13871e76eb989d0b93569463279f2f02b734
naadaa22 <farahmzoughi11@gmail.com>
2025-11-24 22:25:29 +0100

    Littel change on the navbar

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -6,6 +6,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatMenuModule } from '@angular/material/menu';
+import { CartService } from '../../services/cart.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -14,7 +16,41 @@ import { MatMenuModule } from '@angular/material/menu';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   cartItemsCount = 0;
   isLoggedIn = false;
+  userName = '';
+
+  constructor(
+    private cartService: CartService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.updateCartCount();
+    this.updateAuthState();
+
+    this.cartService.getCartItems();
+    
+    this.authService.currentUser$.subscribe(user => {
+      this.isLoggedIn = !!user;
+      this.userName = user?.name || '';
+    });
+  }
+
+  updateCartCount() {
+    this.cartItemsCount = this.cartService.getTotalItems();
+  }
+
+  updateAuthState() {
+    this.isLoggedIn = this.authService.isLoggedIn();
+    const user = this.authService.getCurrentUser();
+    this.userName = user?.name || '';
+  }
+
+  logout() {
+    this.authService.logout();
+    this.isLoggedIn = false;
+    this.userName = '';
+  }
 }
